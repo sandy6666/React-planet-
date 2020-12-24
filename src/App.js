f90a0c0db9ba975d6cfb2283aadbeac09d2e432c
Sandesh S <sandesh.s@codilar.com>
2020-12-24 19:47:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  var planets = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+    "Pluto",
+  ];
+
+  let defaultPlanets = JSON.parse(localStorage.getItem("planets"))
+    ? JSON.parse(localStorage.getItem("planets"))
+    : [];
+
+  const [favouritePlanet, setFavouritePlanet] = useState(defaultPlanets);
+
+  function addPlanetToFaviourite(index) {
+    if (localStorage.getItem("planets") == null) {
+      var planets = [];
+      planets.push(index);
+    }
+    if (localStorage.getItem("planets") != null) {
+      var planets = JSON.parse(localStorage.getItem("planets"));
+      if (!planets.includes(index)) {
+        planets.push(index);
+      }
+    }
+    localStorage.setItem("planets", JSON.stringify(planets));
+    setFavouritePlanet(planets);
+  }
+
+  function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+  function removePlanetToFaviourite(index) {
+    var planets = JSON.parse(localStorage.getItem("planets"));
+    removeItemOnce(planets, index);
+    localStorage.setItem("planets", JSON.stringify(planets));
+    setFavouritePlanet(planets);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="row">
+              <div>Planet List</div>
+            </div>
+            <div>
+              {planets.map((planet, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={() => addPlanetToFaviourite(index)}
+                    className="row mg-5 planet"
+                  >
+                    {planet}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="row">
+              <div>Favourites</div>
+            </div>
+            <div className="fav-planets-parent">
+              {favouritePlanet.map((planet, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={() => removePlanetToFaviourite(planet)}
+                    className="row mg-5 favourite-planets"
+                  >
+                    {planets[planet]}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
